3c02b0bff39830ff23bc3f9e0b55f4e5d9771b0c
Maksim Pelevin <maks.pelevin@gmail.com>
2022-10-12 14:44:42 +0300

    Concrete execution fails for a static method using a static field #711 (#1143)

--- utbot-framework/src/main/kotlin/org/utbot/engine/UtBotSymbolicEngine.kt
+++ utbot-framework/src/main/kotlin/org/utbot/engine/UtBotSymbolicEngine.kt
@@ -497,6 +497,10 @@ class UtBotSymbolicEngine(
         var attempts = 0
         val attemptsLimit = UtSettings.fuzzingMaxAttempts
         val hasMethodUnderTestParametersToFuzz = executableId.parameters.isNotEmpty()
+        if (!hasMethodUnderTestParametersToFuzz && executableId.isStatic) {
+            // Currently, fuzzer doesn't work with static methods with empty parameters
+            return@flow
+        }
         val fuzzedValues = if (hasMethodUnderTestParametersToFuzz) {
             fuzz(methodUnderTestDescription, modelProvider(defaultModelProviders(defaultIdGenerator)))
         } else {
